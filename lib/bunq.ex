b516376663b3b414ca01927cb528c2823a56c86b
Dunya Kirkali <dunyakirkali@gmail.com>
2019-09-19 22:19:33 +0200

    Initiall commit

---
+++ lib/bunq.ex
@@ -0,0 +1,18 @@
+defmodule Bunq do
+  @moduledoc """
+  Documentation for Bunq.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bunq.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bunq_test.exs
@@ -0,0 +1,8 @@
+defmodule BunqTest do
+  use ExUnit.Case
+  doctest Bunq
+
+  test "greets the world" do
+    assert Bunq.hello() == :world
+  end
+end
