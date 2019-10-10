1d792154bcd01bc9ec81e3e0ed3010f9f762eb94
Dunya Kirkali <dunyakirkali@gmail.com>
2019-10-10 22:51:01 +0200

    Correct

--- test/bunq_test.exs
+++ test/bunq_test.exs
@@ -7,7 +7,7 @@ defmodule BunqTest do
     {:ok, priv} = RsaEx.generate_private_key
     {:ok, pub} = RsaEx.generate_public_key(priv)
 
-    # Unauthenticated Client
+    # Client
     client = Bunq.client()
 
     # Create installation
@@ -15,7 +15,7 @@ defmodule BunqTest do
     token = Enum.at(response.body["Response"], 1)["Token"]["token"]
     server_public_key = Enum.at(response.body["Response"], 2)["ServerPublicKey"]["server_public_key"]
 
-    # Authorized Client
+    # Client
     client = Bunq.client(token, priv, server_public_key)
 
     {:ok, response} = Bunq.create_device_server(client)
@@ -24,6 +24,9 @@ defmodule BunqTest do
     {:ok, response} = Bunq.create_session(client)
     token = Enum.at(response.body["Response"], 1)["Token"]["token"]
     user_id = Enum.at(response.body["Response"], 2)["UserPerson"]["id"]
+
+    # Client
+    client = Bunq.client(token, priv, server_public_key)
 
     {:ok, response} = Bunq.get_monetary_accounts(client, user_id)
 
