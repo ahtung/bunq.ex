ee722f3d5ad8bf1e871e2443dfc13f036f8b96c0
Dunya Kirkali <dunyakirkali@gmail.com>
2019-10-13 23:00:57 +0200

    Revert "Add generated models"
    This reverts commit 9f309d56ce760fa2ef1fa0c454dcde59753b93c3.

--- mix.exs
+++ mix.exs
@@ -24,7 +24,6 @@ defmodule Bunq.MixProject do
     [
       {:tesla, "~> 1.2.0"},
       {:hackney, "~> 1.14.0"},
-      {:poison, "~> 3.0"},
       {:jason, ">= 1.0.0"},
       {:rsa_ex, "~> 0.4"},
       {:uuid, "~> 1.1"},
